12d54f2e7c5ded4ec48ffa90ca865006966bdae4
vikas kumar <vk.vikas3003@gmail.com>
2023-03-29 15:12:17 +0530

    active player switch added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // selecting elements
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const current0El = document.querySelector("#current--0");
@@ -9,11 +13,14 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+// initial state
 score0El.textContent = 0;
 score1El.textContent = 0;
-currentScore = 0;
-
 diceEl.classList.add("hidden");
+
+const scores = [0, 0]; // total scores of the 2 players
+let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -24,7 +31,14 @@ btnRoll.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
